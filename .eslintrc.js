82277976895b276d53428f259f51ec3ca0e911e2
Mokhtarz <Mokhtarz@metropolia.fi>
2019-03-22 11:34:43 +0200

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "google",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "rules": {}
+};
